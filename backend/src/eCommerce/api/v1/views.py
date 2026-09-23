7fb2e851f2e1de75c4af3f0b4c868b5f93dc2056
karthik P <karthikkarthikp256@gmail.com>
2026-09-24 00:15:31 +0530

    Added Simple health check api endpoint for test

--- backend/src/eCommerce/api/v1/views.py
+++ backend/src/eCommerce/api/v1/views.py
@@ -1,4 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 def home(request):
     return HttpResponse("Welcome to the eCommerce API!")
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
